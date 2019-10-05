63ffd8f1e67fd8d2b6250186fcdbaaea89df603b
Hajira21 <hmoha096@uottawa.ca>
2019-10-05 05:18:21 -0400

    Added FirebaseAuth;Can now create User and Employee Accounts

--- Android Studio/app/src/main/java/com/example/walkinclinic/SignUpActivity.java
+++ Android Studio/app/src/main/java/com/example/walkinclinic/SignUpActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -18,10 +19,29 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
 public class SignUpActivity extends AppCompatActivity {
     private EditText fieldFirstName, fieldLastName, fieldEmail, fieldPwd, fieldPwdConfirm;
     private RadioGroup fieldUserTypeSelection;
+    private ProgressBar progressBar;
     private char userType;
+    FirebaseAuth mFirebaseAuth;
     private Button buttonSignUp;
     private DatabaseReference ref;
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
@@ -39,77 +59,164 @@ public class SignUpActivity extends AppCompatActivity {
         fieldPwd = findViewById(R.id.password);
         fieldPwdConfirm = findViewById(R.id.passwordConfirm);
         fieldUserTypeSelection = findViewById(R.id.userType);
+        progressBar.setVisibility(View.GONE);
         buttonSignUp = findViewById(R.id.buttonSignUp);
 
-        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.buttonSignUp).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String firstName = fieldFirstName.getText().toString().trim();
-                String lastName = fieldLastName.getText().toString().trim();
-                String email = fieldEmail.getText().toString().trim();
-                String pwd = fieldPwd.getText().toString().trim();
-                String pwdConfirm = fieldPwdConfirm.getText().toString().trim();
+            public void onClick(View v) {
+                if (userType == 'P') {
+                    registerPatient();
+                    //bring to home page
 
-                if (fieldsAreValid(firstName, lastName, email, pwd, pwdConfirm)) {
-                    UserAccount newUser;
-                    if (userType == 'P') {
-                        newUser = new Patient(email, pwd, firstName, lastName, 12345);      // placeholder UID
-                        ref = FirebaseDatabase.getInstance().getReference().child("patients");
-                    }
-                    // userType == 'E'
-                    else {
-                        newUser = new Employee(email, pwd, firstName, lastName, 12345);     // placeholder UID
-                        ref = FirebaseDatabase.getInstance().getReference().child("employees");
-                    }
-                    ref.push().setValue(newUser);
-                    Toast.makeText(SignUpActivity.this, "Registration successful!", Toast.LENGTH_LONG).show();
+                } else {
+                    registerEmployee();
+                    //bring to home page
                 }
             }
         });
+
     }
 
-    public boolean fieldsAreValid(String firstName, String lastName, String email, String pwd, String pwdConfirm) {
-        // check name length
-        if (firstName.length() < 3) {
-            Toast.makeText(SignUpActivity.this, "First name should be at least 3 characters", Toast.LENGTH_LONG).show();
-            return false;
+    private void registerPatient() {
+        final String fname = fieldFirstName.getText().toString().trim();
+        final String lname = fieldLastName.getText().toString().trim();
+        final String email = fieldEmail.getText().toString().trim();
+        final String password = fieldPwd.getText().toString().trim();
+        String cpassword = fieldPwdConfirm.getText().toString().trim();
+
+        if (fname.isEmpty()) {
+            fieldFirstName.setError("Please enter first name");
+            fieldFirstName.requestFocus();
+            return;
         }
-        if (lastName.length() < 2) {
-            Toast.makeText(SignUpActivity.this, "Last name should be at least 2 characters", Toast.LENGTH_LONG).show();
-            return false;
+        if (lname.isEmpty()) {
+            fieldLastName.setError("Please enter last name");
+            fieldLastName.requestFocus();
+            return;
         }
-        // check email pattern
-        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(email);
-        if (! matcher.find()) {
-            Toast.makeText(SignUpActivity.this, "Enter a valid email", Toast.LENGTH_LONG).show();
-            return false;
+
+        if (email.isEmpty()) {
+            fieldEmail.setError("Please enter email address");
+            fieldEmail.requestFocus();
+            return;
         }
-        // check password length and match
-        if (pwd.length() < 6 || pwdConfirm.length() < 6) {
-            Toast.makeText(SignUpActivity.this, "Password must be at least 6 characters", Toast.LENGTH_LONG).show();
-            return false;
+
+        if (password.isEmpty()) {
+            fieldPwd.setError("Please enter a password");
+            fieldPwd.requestFocus();
+            return;
         }
-        if (! pwd.equals(pwdConfirm)) {
+
+        if (!password.equals(cpassword)) {
             Toast.makeText(SignUpActivity.this, "Passwords must match", Toast.LENGTH_LONG).show();
-            return false;
-        }
-        // check if user type is selected
-        if (fieldUserTypeSelection.getCheckedRadioButtonId() == -1) {
-            Toast.makeText(SignUpActivity.this, "Select your status", Toast.LENGTH_LONG).show();
-            return false;
+            return;
         }
 
-        return true;
+        progressBar.setVisibility(View.VISIBLE);
+        mFirebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Patient patient = new Patient(email, password, fname, lname);
+
+                            FirebaseDatabase.getInstance().getReference("Patient")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(patient).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    progressBar.setVisibility(View.GONE);
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(SignUpActivity.this, "User Registered !", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        //display a failure message
+                                    }
+                                }
+
+                            });
+                        } else {
+                            //display a failure message
+                        }
+                    }
+                });
     }
+
+    private void registerEmployee() {
+        final String fname = fieldFirstName.getText().toString().trim();
+        final String lname = fieldLastName.getText().toString().trim();
+        final String email = fieldEmail.getText().toString().trim();
+        final String password = fieldPwd.getText().toString().trim();
+        String cpassword = fieldPwdConfirm.getText().toString().trim();
+
+        if (fname.isEmpty()) {
+            fieldFirstName.setError("Please enter first name");
+            fieldFirstName.requestFocus();
+            return;
+        }
+        if (lname.isEmpty()) {
+            fieldLastName.setError("Please enter last name");
+            fieldLastName.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()) {
+            fieldEmail.setError("Please enter email address");
+            fieldEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            fieldPwd.setError("Please enter a password");
+            fieldPwd.requestFocus();
+            return;
+        }
+
+        if (!password.equals(cpassword)) {
+            Toast.makeText(SignUpActivity.this, "Passwords must match", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mFirebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Employee employee = new Employee(email, password, fname, lname);
+
+                            FirebaseDatabase.getInstance().getReference("Patient")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(employee).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    progressBar.setVisibility(View.GONE);
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(SignUpActivity.this, "Walk in Clinic Registered !", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(SignUpActivity.this, "Walk in Clinic Not Registered !", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+
+                            });
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Walk in Clinic Not Registered !", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
 
     public void setUserType(View v) {
         int selectedID = fieldUserTypeSelection.getCheckedRadioButtonId();
 
         if (selectedID == R.id.isPatient) {
             userType = 'P';
-        }
-        else if (selectedID == R.id.isClinicEmployee) {
+        } else if (selectedID == R.id.isClinicEmployee) {
             userType = 'E';
         }
     }
+
+
 }
--- Android Studio/app/src/main/java/com/example/walkinclinic/account/Employee.java
+++ Android Studio/app/src/main/java/com/example/walkinclinic/account/Employee.java
@@ -1,7 +1,7 @@
 package com.example.walkinclinic.account;
 
 public class Employee extends UserAccount {
-    public Employee(String email, String password, String nameFirst, String nameLast, int UID) {
-        super(email, password, nameFirst, nameLast, UID);
+    public Employee(String email, String password, String nameFirst, String nameLast) {
+        super(email, password, nameFirst, nameLast);
     }
 }
